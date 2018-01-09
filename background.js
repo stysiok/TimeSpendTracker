3d693f7964db69ecbd7c87f17e828b366257f395
Grzegorz <stysiok@o2.pl>
2018-01-09 19:30:44 +0100

    #2 - Created objects and basics of time tracking
    Bugs:
    - time diffrence needs to be function
    - unique urls array checking
    - adding time to existing website

--- background.js
+++ background.js
@@ -1,14 +1,40 @@
-var uniqueUrls = [];
+class Website{
+    constructor(iconUrl, title, url, entryTime, exitTime){
+        this.iconUrl = iconUrl,
+        this.title = title,
+        this.url = url,
+        this.timeArray = new Array(new TimeSpend(entryTime, exitTime))
+    }
+}
 
+class TimeSpend{
+    constructor(entryTime, exitTime){
+        this.entryTime = entryTime,
+        this.exitTime = exitTime
+    }
+    get duration(){
+        this.calcDuration();
+    }
+
+    //change to function
+    calcDuration(){
+        return moment(this.exitTime).substract(this.entryTime);
+    }
+}
+
+var uniqueUrls = [];
+var websites = [];
+var currentWebsite;
+var urlRegex = /[a-z0-9-\.]+\.[a-z]{2,4}\/?([^\s<>\#%"\,\{\}\\|\\\^\[\]`]+)?$/;
 
 //--Chrome Extension section
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    if(changeInfo.status !== "complete")
+    if(tab.status !== "complete" || !tab.active || changeInfo.status === undefined)
         return;
-    
-    console.log("Tab has been updated");
+
+    console.log(`Tab has been updated`);
     //console.log(tab);
-    addUniqueUrl(tab.url);
+    tabChangedOrUpdated(tab);
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo){
@@ -20,9 +46,9 @@ chrome.tabs.onActivated.addListener(function(activeInfo){
 
 function getCurrentSelectedTab(tabId){
     chrome.tabs.get(tabId, function(tab) {
-        console.log("selected tab changed");
+        console.log(`selected tab has changed`);
         //console.log(tab);
-        addUniqueUrl(tab.url);
+        tabChangedOrUpdated(tab);
     });
 }
 
@@ -31,26 +57,52 @@ function urlParser(url){
     var element = document.createElement('a');
     element.href = url;
 
-    var hostname = element.hostname;
-    console.log(hostname);
-
-    return hostname;
+    return element.hostname;
 }
 
-function addUniqueUrl(url){
-    var parsedUrl = urlParser(url);
+//--add website or time to website
+function tabChangedOrUpdated(tab){
+    let parsedUrl = urlParser(tab.url);
+    
+    //scenariusz ze strony np. do historii, nowej karty itd.
+    if(!urlRegex.test(parsedUrl)){
+        console.log(`urlError: ${parsedUrl}`);
+        return;
+    }
 
-    if(uniqueUrls.indexOf(parsedUrl) === -1){
-        uniqueUrls.push(parsedUrl);
-        console.log(`Url is unique and has been added to array (${parsedUrl})`);
+    if(currentWebsite == null){
+        currentWebsite = new Website(tab.favIconUrl, tab.title, parsedUrl, moment().format('LTS'), null);
+        return;
     }
-    else{
-        console.log(`Url isn't unique. (${parsedUrl})`);
+    else if(currentWebsite.url === parsedUrl){
+        return;
     }
+
+    currentWebsite.timeArray[0].exitTime = moment().format('LTS');
+    console.log("Byłem na poniższej stronie");
+    console.log(currentWebsite);
+
+    //tu nie działa dodawanie nowych stron
+    if(uniqueUrls.indexOf(currentWebsite.url) === -1){
+        uniqueUrls.push(parsedUrl);    
+        websites.push(currentWebsite);
+    }else{
+        addTimeToExisingWebsite(tab);
+    }
+
+    currentWebsite = new Website(tab.favIconUrl, tab.title, parsedUrl, moment().format('LTS'), null);
 }
 
-function showAllUniqueUrls(){
-    uniqueUrls.forEach(element => {
-        console.log(element);
-    });
+
+//to be continued...
+function addTimeToExisingWebsite(){
+    let index = websites.findIndex(element => element.url == currentWebsite.url);
+    
+    if(index === -1){
+        console.log("coś poszło nie tak przy sprawdzaniu czy strona istnieje w bazie");
+        return;
+    }
+
+    var timeTracked = new TimeSpend(currentWebsite.entryTime, currentWebsite.exitTime);
+    websites[index].timeArray.push(timeTracked);
 }
