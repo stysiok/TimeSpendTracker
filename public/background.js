8f35c8d425aff72df5bc8ebdc2896565800bf7ce
Grzegorz <stysiok@o2.pl>
2018-01-23 19:43:54 +0100

    #8 - Set and Get data from Chrome Storage
    DONE:
    - Setting data to specyfic key
    - Getting data from specyfic key
    - Minor format changes
    TO DO:
    - Sending data to view
    - Getting request from view
    - Create view with React, ChartJS and Bootstrap

--- public/background.js
+++ public/background.js
@@ -1,34 +1,38 @@
-class Website{
-    constructor(url, iconUrl, entryTime, exitTime){
+class Website {
+    constructor(url, iconUrl, entryTime, exitTime) {
         this.url = url,
-        this.iconUrl = iconUrl,
-        this.timeArray = new Array(new TimeSpend(entryTime, exitTime))
+            this.iconUrl = iconUrl,
+            this.timeArray = new Array(new TimeSpent(entryTime, exitTime))
     }
 
-    calcTimeDuration(format){
+    get wholeDuration() {
+        return this.calcTimeDuration(false);
+    }
+
+    calcTimeDuration(format) {
         var result = 0;
-        for(var i = 0; i < this.timeArray.length; i++){
+        for (var i = 0; i < this.timeArray.length; i++) {
             result += this.timeArray[i].duration;
         }
 
-        return format ? moment(result).utcOffset(0).format("HH:mm:ss") : result;            
+        return format ? moment(result).utcOffset(0).format("HH:mm:ss") : result;
     }
 }
 
-class TimeSpend{
-    constructor(entryTime, exitTime){
+class TimeSpent {
+    constructor(entryTime, exitTime) {
         this.entryTime = entryTime,
-        this.exitTime = exitTime
+            this.exitTime = exitTime
     }
 
-    get duration(){
+    get duration() {
         return this.calcDuration();
     }
 
-    calcDuration(format){
+    calcDuration(format) {
         var entry = moment(this.entryTime, "HH:mm:ss");
         var exit = moment(this.exitTime, "HH:mm:ss");
-        
+
         return exit.diff(entry);
     }
 }
@@ -39,8 +43,8 @@ var currentWebsite;
 var urlRegex = /[a-z0-9-\.]+\.[a-z]{2,4}\/?([^\s<>\#%"\,\{\}\\|\\\^\[\]`]+)?$/;
 
 //--Chrome Extension section
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    if(tab.status !== "complete" || !tab.active || changeInfo.status === undefined)
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (tab.status !== "complete" || !tab.active || changeInfo.status === undefined)
         return;
 
     console.log(`Tab has been updated`);
@@ -48,15 +52,15 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     tabChangedOrUpdated(tab);
 });
 
-chrome.tabs.onActivated.addListener(function(activeInfo){
-    if(activeInfo.tabId == "")
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    if (activeInfo.tabId == "")
         return;
 
     getCurrentSelectedTab(activeInfo.tabId);
 });
 
-function getCurrentSelectedTab(tabId){
-    chrome.tabs.get(tabId, function(tab) {
+function getCurrentSelectedTab(tabId) {
+    chrome.tabs.get(tabId, function (tab) {
         console.log(`selected tab has changed`);
         console.log(tab);
         tabChangedOrUpdated(tab);
@@ -64,7 +68,7 @@ function getCurrentSelectedTab(tabId){
 }
 
 //--functions of URLs
-function urlParser(url){
+function urlParser(url) {
     var element = document.createElement('a');
     element.href = url;
 
@@ -72,118 +76,99 @@ function urlParser(url){
 }
 
 //--add new website or time to website to website array
-function tabChangedOrUpdated(tab){
+function tabChangedOrUpdated(tab) {
     var parsedUrl = urlParser(tab.url);
     var isBadUrl = !urlRegex.test(parsedUrl);
 
-    if(isBadUrl && currentWebsite == null){
+    if (isBadUrl && currentWebsite == null) {
         return;
     }
-    else if(currentWebsite == null){
+    else if (currentWebsite == null) {
         currentWebsite = new Website(parsedUrl, tab.favIconUrl, getCurrentTime(), null);
         return;
     }
-    else if(currentWebsite.url === parsedUrl){
+    else if (currentWebsite.url === parsedUrl) {
         return;
     }
 
     currentWebsite.timeArray[0].exitTime = getCurrentTime();
-    
-    if(uniqueUrls.indexOf(currentWebsite.url) === -1){
-        uniqueUrls.push(currentWebsite.url);    
+
+    if (uniqueUrls.indexOf(currentWebsite.url) === -1) {
+        uniqueUrls.push(currentWebsite.url);
         websites.push(currentWebsite);
-    }else{
+    } else {
         addTimeToExisingWebsite();
     }
 
-    if(isBadUrl){
-        currentWebsite = null;    
-    }else{
+    if (isBadUrl) {
+        currentWebsite = null;
+    } else {
         currentWebsite = new Website(parsedUrl, tab.favIconUrl, getCurrentTime(), null);
     }
 }
 
-function addTimeToExisingWebsite(){
+function addTimeToExisingWebsite() {
     var index = websites.findIndex(element => element.url == currentWebsite.url);
-    
-    if(index === -1){
+
+    if (index === -1) {
         console.log("coś poszło nie tak przy sprawdzaniu czy strona istnieje w bazie");
         return;
     }
 
-    var timeTracked = new TimeSpend(currentWebsite.timeArray[0].entryTime, currentWebsite.timeArray[0].exitTime);
+    var timeTracked = new TimeSpent(currentWebsite.timeArray[0].entryTime, currentWebsite.timeArray[0].exitTime);
     websites[index].timeArray.push(timeTracked);
 }
 
 //--saving data
 //https://developer.chrome.com/extensions/storage
 //http://julip.co/2010/01/how-to-build-a-chrome-extension-part-2-options-and-localstorage/
-chrome.windows.onRemoved.addListener(function(windowId){
-    chrome.windows.getAll(function(windows){
-        if(windows.length != 0)
+chrome.windows.onRemoved.addListener(function (windowId) {
+    chrome.windows.getAll(function (windows) {
+        if (windows.length != 0)
             return;
-        
-       saveDataToStorage();
+
+        //saveDataToStorage();
     })
 })
 
-function saveDataToStorage(){
-    //getDataFromStorage
-    //function to get keys for period of time
-    var key = moment().format("DD-MM-YYYY");
-
-    chrome.storage.sync.get(key, function(items){
-        for(var i = 0; i < items.length; i++){
-            var index = websites.findIndex(element => element.url == items[i].url);
-
-            if(index !== -1){
-                var times = items[i].timeArray;
-                for(var j = 0; j < times.length; j++){
-                    //needs to be sorted bcs time will be ealier than current time
-                    websites[index].timeArray.push(new TimeSpend(times[j].entryTime, times[j].exitTime));
-                }
-
-                items.splice(i, 1);
-            }
-        }
-        
-        if(items.length != 0){
-            //map rest of the data from storage to Website's objects
-        }
-    });
-    //map
-    //saveDataToStorage
-}
-
+//temporary solution - need to think how to implement a better constructor for website class
 var mapItemsToWebsites = (items) => {
     var mapedWebsites = [];
-    for(var i = 0; i < items.length; i++){
-        var website = new Website();
+    for (var i = 0; i < items.length; i++) {
+        var temp = items[i];
+        var website = new Website(temp.url, temp.iconUrl, temp.timeArray[0].entryTime, temp.timeArray[0].exitTime);
 
-        mappedWebsites.push(website);
+        if(temp.timeArray.length > 1){
+            for (var j = 1; j < temp.timeArray.length; j++) {
+                var tempTS = new TimeSpent(temp.timeArray[j].entryTime, temp.timeArray[j].exitTime);
+                website.timeArray.push(tempTS);
+            }
+        }
+
+        mapedWebsites.push(website);
     }
 
     return mapedWebsites;
 }
 
 //-- additional MomentJS functions
-function getCurrentTime(){
+function getCurrentTime() {
     return moment().format("HH:mm:ss");
 }
 
-function parseToHHmmss(value){
+function parseToHHmmss(value) {
     return moment(value).utcOffset(0).format("HH:mm:ss");
 }
 
-//--Checking functions
-function logTimeForWebsite(index){
+//--Checking functions & Tests
+function logTimeForWebsite(index) {
     var result = websites[index].calcTimeSpend();
     return parseToHHmmss(result);
 }
 
-function logTimeForAllWebsites(){
+function logTimeForAllWebsites() {
     var arr = [];
-    for(var i = 0; i < websites.length; i++){
+    for (var i = 0; i < websites.length; i++) {
         var timeS = logTimeForAllWebsites(i);
 
         var obj = {
@@ -194,4 +179,108 @@ function logTimeForAllWebsites(){
         arr.push(obj);
     }
     console.log(arr);
+}
+
+function mapItemsToWebsiteObjectsTest() {
+    var obj1 = {
+        url: "das",
+        iconUrl: "123",
+        timeArray: [
+            {
+                entryTime: "12:12:12",
+                exitTime: "12:32:12"
+
+            },
+            {
+                entryTime: "12:33:33",
+                exitTime: "14:12:32"
+
+            }
+        ]
+    }
+    var obj2 = {
+        url: "da321s",
+        iconUrl: "112312323",
+        timeArray: [
+            {
+                entryTime: "12:12:12",
+                exitTime: "12:32:12"
+
+            },
+            {
+                entryTime: "12:33:33",
+                exitTime: "14:12:32"
+
+            }
+        ]
+    }
+    var obj3 = {
+        url: "das12",
+        iconUrl: "112312312323",
+        timeArray: [
+            {
+                entryTime: "12:12:12",
+                exitTime: "12:32:12"
+
+            },
+            {
+                entryTime: "12:33:33",
+                exitTime: "14:12:32"
+
+            }
+        ]
+    }
+
+    var objs = [obj1, obj2, obj3];
+
+    var mapped = mapItemsToWebsites(objs);
+    console.log(objs);
+    console.log(mapped);
+}
+
+function getData(key) {
+    chrome.storage.sync.get([key], function (items) {
+        var toMap = [];
+        console.log(items);
+        var tItems = items[key];
+
+        for (var i = 0; i < tItems.length; i++) {
+            var index = websites.findIndex(element => element.url == tItems[i].url);
+
+            if (index !== -1) {
+                var times = tItems[i].timeArray;
+                for (var j = 0; j < times.length; j++) {
+                    //needs to be sorted bcs time will be ealier than current time
+                    websites[index].timeArray.push(new TimeSpent(times[j].entryTime, times[j].exitTime));
+                }
+
+                websites[i].timeArray.sort((obj1, obj2) => obj1.entryTime > obj2.entryTime);
+            }
+            else {
+                toMap.push(tItems[i]);
+            }
+        }
+
+        if (tItems.length != 0) {
+            websites = websites.concat(mapItemsToWebsites(toMap));
+        }
+    });
+    console.log(websites);
+}
+
+function saveDataToStorage(key) {
+    //getDataFromStorage
+    //function to get keys for period of time
+
+    //var saveKey = moment().format("DD-MM-YYYY");
+    var obj = {
+        [key]: websites
+    };
+    console.log(obj);
+
+    chrome.storage.sync.set(obj, function () {
+        console.log("Saved data");
+        websites = [];
+        uniqueUrls = [];
+    });
 }
